a9a06b2221ef3d184f6455f5f83b666252f64132
Vipul-semwal <vipulsemwal124@gmail.com>
2024-06-25 15:12:55 +0530

    ejs pages added for demp

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('SignupSucc', { data: "vipul", url: process.env.CLIENTHOMEURL });
+});
+
+router.get('/error', function (req, res, next) {
+  res.render('SignupError', { error: { message: 'Something went wrong! Please Sign Up Again', url: process.env.ClientBaseUrl } });
 });
 
 module.exports = router;
